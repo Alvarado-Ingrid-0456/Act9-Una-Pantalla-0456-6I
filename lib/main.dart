e109ae74d1151cda1888d3bb3bc1e1603943be9a
a.23308051280456 <a.23308051280456@cbtis128.edu.mx>
2026-02-17 16:26:11 +0000

    Update project files

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MiCelular());
+  runApp(const MiCelular());
 }
 
 class MiCelular extends StatelessWidget {
+  const MiCelular({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: PantallaInicio(),
     );
@@ -15,152 +17,171 @@ class MiCelular extends StatelessWidget {
 }
 
 class PantallaInicio extends StatelessWidget {
+  const PantallaInicio({super.key});
 
   final String fondoUrl =
-      "https://raw.githubusercontent.com/Alvarado-Ingrid-0456/Imagenes-para-Flutter-6to-I-Fecha-11-Feb-2026/refs/heads/main/ajolote.png";
+      "https://raw.githubusercontent.com/Alvarado-Ingrid-0456/Imagenes-para-Flutter-6to-I-Fecha-11-Feb-2026/refs/heads/main/fondo.jpg";
 
   final String logoCometaUrl =
       "https://raw.githubusercontent.com/Alvarado-Ingrid-0456/Imagenes-para-Flutter-6to-I-Fecha-11-Feb-2026/refs/heads/main/logo.jpg";
 
   @override
   Widget build(BuildContext context) {
+    double ancho = MediaQuery.of(context).size.width;
+    double tamanoIcono = ancho > 600 ? 50 : 35;
+
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
+      body: Stack(
+        children: [
 
-          double ancho = constraints.maxWidth;
-
-          int columnas = 4;
-          if (ancho > 600 && ancho <= 1000) {
-            columnas = 6;
-          } else if (ancho > 1000) {
-            columnas = 8;
-          }
-
-          return Stack(
-            children: [
-
-              // Fondo
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(fondoUrl),
-                    fit: BoxFit.cover,
-                  ),
-                ),
+          /// Fondo
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(fondoUrl),
+                fit: BoxFit.cover,
               ),
+            ),
+          ),
 
-              SafeArea(
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
                 child: Column(
                   children: [
 
-                    // Widget Clima
-                    Container(
-                      margin: EdgeInsets.all(20),
-                      padding: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        color: Colors.black54,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.wb_sunny,
-                            color: Colors.yellow,
-                            size: ancho > 600 ? 50 : 35,
+                    /// FILA 1
+                    Row(
+                      children: [
+
+                        /// Clima
+                        Expanded(
+                          child: Container(
+                            padding: const EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              color: Colors.black54,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Row(
+                              children: [
+                                Icon(Icons.nightlight_round,
+                                    color: Colors.white,
+                                    size: tamanoIcono),
+                                const SizedBox(width: 10),
+                                const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "7°",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 22),
+                                    ),
+                                    Text(
+                                      "Nublado",
+                                      style:
+                                          TextStyle(color: Colors.white70),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
                           ),
-                          SizedBox(width: 15),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "28°C",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: ancho > 600 ? 28 : 20,
-                                ),
-                              ),
-                              Text(
-                                "Soleado - México",
-                                style: TextStyle(
-                                  color: Colors.white70,
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
+                        ),
+
+                        const SizedBox(width: 20),
+
+                        iconoApp(Icons.shop, "Play Store", tamanoIcono),
+                      ],
                     ),
 
-                    // Grid ocupa el espacio restante
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: GridView.count(
-                          crossAxisCount: columnas,
-                          mainAxisSpacing: 25,
-                          crossAxisSpacing: 25,
+                    const SizedBox(height: 40),
+
+                    /// FILA 2
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        iconoApp(Icons.photo, "Galería", tamanoIcono),
+                        iconoApp(Icons.calendar_month, "Calendario", tamanoIcono),
+                        iconoApp(Icons.settings, "Ajustes", tamanoIcono),
+                      ],
+                    ),
+
+                    const SizedBox(height: 40),
+
+                    /// FILA 3
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        iconoApp(Icons.access_time, "Reloj", tamanoIcono),
+                        iconoApp(Icons.image_search, "Pinterest", tamanoIcono),
+                        iconoApp(Icons.sports_esports, "Play Juegos", tamanoIcono),
+                      ],
+                    ),
+
+                    const SizedBox(height: 40),
+
+                    /// FILA 4
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+
+                        iconoApp(Icons.shopping_cart, "Amazon", tamanoIcono),
+
+                        Column(
                           children: [
-
-                            iconoApp(Icons.shop, "Play Store"),
-                            iconoApp(Icons.photo, "Galería"),
-                            iconoApp(Icons.calendar_month, "Calendario"),
-                            iconoApp(Icons.settings, "Ajustes"),
-                            iconoApp(Icons.access_time, "Reloj"),
-                            iconoApp(Icons.image_search, "Pinterest"),
-                            iconoApp(Icons.sports_esports, "Play Juegos"),
-                            iconoApp(Icons.shopping_cart, "Amazon"),
-
-                            // Papelería Cometa
-                            Column(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(15),
-                                  child: Image.network(
-                                    logoCometaUrl,
-                                    height: ancho > 600 ? 70 : 50,
-                                    width: ancho > 600 ? 70 : 50,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                SizedBox(height: 5),
-                                Text(
-                                  "Papelería Cometa",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: ancho > 600 ? 12 : 10,
-                                  ),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ],
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.network(
+                                logoCometaUrl,
+                                height: tamanoIcono + 10,
+                                width: tamanoIcono + 10,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            const Text(
+                              "Papelería Cometa",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
+                              textAlign: TextAlign.center,
                             ),
                           ],
                         ),
-                      ),
+                      ],
                     ),
                   ],
                 ),
               ),
-            ],
-          );
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Widget iconoApp(IconData icono, String nombre) {
+  Widget iconoApp(IconData icono, String nombre, double tamano) {
     return Column(
       children: [
-        Icon(
-          icono,
-          size: 45,
-          color: Colors.white,
+        Container(
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: Colors.black45,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Icon(
+            icono,
+            size: tamano,
+            color: Colors.white,
+          ),
         ),
-        SizedBox(height: 5),
+        const SizedBox(height: 5),
         Text(
           nombre,
-          style: TextStyle(
+          style: const TextStyle(
             color: Colors.white,
             fontSize: 12,
           ),
